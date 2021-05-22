6793a5f40772bf2a7bbbab451e2958ac43a9b06d
Yogesh Bamanier <bamanieryogesh1@gmail.com>
2021-05-23 04:47:38 +0530

    Updated some things

--- src/App.js
+++ src/App.js
@@ -1,154 +1,163 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const listStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  margin: '5px 20% 5px 20%',
-  border: '2px solid  #944dff',
-  minHeight: '200px',
-  backgroundColor: '#e6e6ff'
-}
+  display: "flex",
+  flexDirection: "column",
+  margin: "5px 20% 5px 20%",
+  border: "2px solid  #944dff",
+  minHeight: "200px",
+  backgroundColor: "#e6e6ff"
+};
 
 const inputStyle = {
-  width: '30%',
-  border: '2px solid grey',
-  borderRadius: '5px',
-  marginRight: '2px',
-  height: '22px',
-  padding: '0 0 0 5px'
-}
+  width: "30%",
+  border: "2px solid grey",
+  borderRadius: "5px",
+  marginRight: "2px",
+  height: "22px",
+  padding: "0 0 0 5px"
+};
 
 const ulStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  listStyleType: 'none',
-  margin: '2px 2px 2px 2px',
-  padding: '0 0 0 20px'
-}
+  display: "flex",
+  flexDirection: "row",
+  listStyleType: "none",
+  margin: "2px 2px 2px 2px",
+  padding: "0 0 0 20px"
+};
 const doneButton = {
-  border: '1px solid grey',
-  backgroundColor: '#6600ff',
-  borderRadius: '5px',
-  color: 'white',
-  margin:'0 5px 0 auto'
-}
+  border: "1px solid grey",
+  backgroundColor: "#6600ff",
+  borderRadius: "5px",
+  color: "white",
+  margin: "0 5px 0 auto"
+};
 
 const button = {
-  border: '1px solid grey',
-  backgroundColor: '#6600ff',
-  borderRadius: '5px',
-  color: 'white', fontSize: '14px',
-  height: '26px',
-}
+  border: "1px solid grey",
+  backgroundColor: "#6600ff",
+  borderRadius: "5px",
+  color: "white",
+  fontSize: "14px",
+  height: "26px"
+};
 
 const list = {
-  fontSize: '16px',
-  color: '#3333ff',
-  fontWeight: '400',
-  padding: '5px 0 0 0'
-}
-
+  fontSize: "16px",
+  color: "#3333ff",
+  fontWeight: "400",
+  padding: "5px 0 0 0"
+};
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       currentTodo: {
-        text: '',
-        isActive: true
+        text: "",
+        id: 0
       },
       todoArr: [],
 
       deletedArr: []
-
-    }
+    };
   }
 
-
   onSubmit = (e) => {
+    const { currentTodo , todoArr } = this.state;
     e.preventDefault();
-    if (this.state.currentTodo.text === '') {
+    if (currentTodo.text === "") {
       alert("Please Enter Some Text.");
+    } else {
+     const updatedTodoArr = [...todoArr , currentTodo];
 
-    }
-    else {
-      this.state.todoArr.push(this.state.currentTodo);
-
-      this.setState({ currentTodo: { text: '' }, todoArr: this.state.todoArr })
-
-      console.log(this.state.todoArr)
-
+      this.setState({
+        currentTodo: { text: "", id: 0 },
+        todoArr: updatedTodoArr
+      });
+      console.log(currentTodo);
+      console.log(todoArr);
     }
   };
 
   onChange = (e) => {
-    this.setState({ currentTodo: { text: e.target.value, isActive: true } });
-    if (e.target.value === '') {
-      console.log('err//');
+    this.setState({
+      currentTodo: { text: e.target.value, id: this.state.todoArr.length }
+    });
+    if (e.target.value === "") {
+      console.log("err//");
     }
   };
 
-
-  remove = key => {
-    const filteredItems = this.state.todoArr.filter(todo => {
-      return todo.text !== key
+  remove = (key) => {
+    const filteredItems = this.state.todoArr.filter((todo) => {
+      return todo.id !== key;
     });
-    const deletedItem = this.state.todoArr.filter(todo => {
-      return todo.text === key
+    const deletedItem = this.state.todoArr.filter((todo) => {
+      return todo.id === key;
     });
-    const removedItem = this.state.deletedArr.concat(deletedItem)
+    const removedItem = this.state.deletedArr.concat(deletedItem);
     this.setState({
-      todoArr: filteredItems, deletedArr: removedItem
-    })
-    console.log(this.state.deletedArr);
+      todoArr: filteredItems,
+      deletedArr: removedItem
+    });
   };
 
-
   render() {
+    const {
+      currentTodo: { text },
+      todoArr,
+      deletedArr
+    } = this.state;
     return (
-
       <div className="App">
-        <header className="App-header">
-          todo App
-      </header>
+        <header className="App-header">todo App</header>
         <div>
           <form onSubmit={this.onSubmit}>
-            <input name='text' type='text'
-              placeholder='Enter task here... '
-              value={this.state.currentTodo.text} autoComplete='off'
+            <input
+              name="text"
+              type="text"
+              placeholder="Enter task here... "
+              value={text}
+              autoComplete="off"
               onChange={this.onChange}
-              style={inputStyle} />
+              style={inputStyle}
+            />
 
-            <input type='submit' value='Add Task' style={button} />
+            <input type="submit" value="Add Task" style={button} />
           </form>
         </div>
-        {this.state.todoArr.length === 0 ? (<p style={{ fontWeight: '300', marginTop: '10%' }}>
-          Type your tasks for today...</p>
+        {todoArr.length === 0 ? (
+          <p style={{ fontWeight: "300", marginTop: "10%" }}>
+            Type your tasks for today...
+          </p>
         ) : (
-            <div style={listStyle}>
-              {this.state.todoArr.map((todo) =>
-                todo.isActive && <ul style={ulStyle}>
-                  <li key={todo.text} style={list}>{todo.text}</li>
-                  <button style={doneButton} onClick={() => this.remove(todo.text)}>Done</button>
-                </ul>)}
+          <div style={listStyle}>
+            {todoArr.map((todo) => (
+              <ul key={todo.id} style={ulStyle}>
+                <li key={todo.text} style={list}>
+                  {todo.text}
+                </li>
+                <button style={doneButton} onClick={() => this.remove(todo.id)}>
+                  Done
+                </button>
+              </ul>
+            ))}
+          </div>
+        )}
 
-            </div>)}
-
-        <div style={{ margin: '40px' }}>
-
-          {this.state.deletedArr.map((element) =>
-            (<ul>
-              <li key={element.text}>completed task: {element.text}</li></ul>)
-          )}
+        <div style={{ margin: "40px" }}>
+          {deletedArr.map((element) => (
+            <ul>
+              <li key={element.text}>completed task: {element.text}</li>
+            </ul>
+          ))}
         </div>
       </div>
-
     );
   }
-
 }
 
 export default App;
